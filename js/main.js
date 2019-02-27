d58e5141dd05f42de1a0a84899ab2f652fcf23ee
John Skeen <jskeen@gmail.com>
2019-02-27 10:20:35 -0500

    Minor changes this morning
    Big changes coming up

--- js/main.js
+++ js/main.js
@@ -10,3 +10,32 @@ $("a[href^='#']").click(function(e) {
 
 	$("body, html").animate({scrollTop: position}, 1000 );
 });
+
+// Open the Modal
+function openModal() {
+  document.getElementById('modal').style.display = "block";
+}
+
+// Close the Modal
+$('#close').click(function(){
+  $('#modal').hide();
+});
+
+var projectIndex = 1;
+showProjects(projectIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showProjects(projectIndex += n);
+}
+
+function showProject(n) {
+  var i;
+  var project = document.getElementsByClassName("project");
+  if (n > project.length) {projectIndex = 1}
+  if (n < 1) {projectIndex = project.length}
+  for (i = 0; i < project.length; i++) {
+    project[i].style.display = "none";
+  }
+  project[projectIndex-1].style.display = "block";
+}
