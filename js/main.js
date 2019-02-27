c106953f1a7c36d1282aa186ab639af5b7ce7cf8
John Skeen <jskeen@gmail.com>
2019-02-27 15:00:43 -0500

    Fixed modal

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@
 
 $("a[href^='#']").click(function(e) {
 	e.preventDefault();
-  $('.home').css('overflow', 'auto');
 	$('#burger').prop('checked', false);
 
 	var position = $($(this).attr("href")).offset().top;
@@ -11,31 +10,139 @@ $("a[href^='#']").click(function(e) {
 	$("body, html").animate({scrollTop: position}, 1000 );
 });
 
-// Open the Modal
-function openModal() {
-  document.getElementById('modal').style.display = "block";
+// Modal JS
+
+const portfolio = document.getElementById('portfolio');
+const modal = document.getElementById('modal');
+const close = document.getElementById('close');
+const projectInfoApi = 'https://raw.githubusercontent.com/JohnSkeen/johnskeen.github.io/master/projects.json';
+let projectData = [];
+let currentIndex = 0;
+
+// ------------------------------------------
+//  FETCH FUNCTIONS
+// ------------------------------------------
+
+function fetchData(url) {
+  return fetch(url)
+            .then(checkStatus)
+            .then(res => res.json())
+            .catch(error => console.log('Looks like there was a problem!', error));
 }
 
-// Close the Modal
-$('#close').click(function(){
-  $('#modal').hide();
-});
+fetchData(projectInfoApi)
+  .then(data => {
+    let projectInfo = "<ul class=\"grid-container\">";
 
-var projectIndex = 1;
-showProjects(projectIndex);
+    $.each (data, function (key, value) {
+      projectInfo += `
+      <li class="${value.class} project" data-index="` + key + `">
+        ${value.thumbnail}
+      </li>`
 
-// Next/previous controls
-function plusSlides(n) {
-  showProjects(projectIndex += n);
-}
+      let portfolioResults = {
+        title: value.title,
+        challenge: value.challenge,
+        skill1: value.skill1,
+        skill2: value.skill2,
+        skill3: value.skill3,
+        skill4: value.skill4,
+        fullImage: value.fullImage,
+        thumbnail: value.thumbnail,
+        class: value.class,
+        githubLink: value.githubLink,
+        liveLink: value.liveLink
+      };
+      projectData.push(portfolioResults);
+    });
+    projectInfo += `</ul>`;
+    portfolio.innerHTML = projectInfo;
+  })
 
-function showProject(n) {
-  var i;
-  var project = document.getElementsByClassName("project");
-  if (n > project.length) {projectIndex = 1}
-  if (n < 1) {projectIndex = project.length}
-  for (i = 0; i < project.length; i++) {
-    project[i].style.display = "none";
-  }
-  project[projectIndex-1].style.display = "block";
-}
+	// ------------------------------------------
+	//  HELPER FUNCTIONS
+	// ------------------------------------------
+
+	function checkStatus(response) {
+	  if(response.ok) {
+	    return Promise.resolve(response);
+	  } else {
+	    return Promise.reject(new Error(response.statusText));
+	  }
+	}
+
+	function scrollCheck(){
+		if (currentIndex == 0) {
+			$('#scrollLeft').hide();
+		} else if (currentIndex == 4) {
+			$('#scrollRight').hide();
+		} else {
+			$('#scrollRight').show();
+			$('#scrollLeft').show();
+		}
+	}
+
+	// Get Modal Element
+	function viewModal(index){
+	  let currentProject = projectData[index];
+
+	  modalInner.innerHTML = `
+		<span id="close"><svg id="close-button"><use xlink:href='#closebutton'></use></svg></span>
+		<span id="scrollLeft"><svg id="left-arrow"><use xlink:href="#leftarrow"></use></svg></span>
+		<span id="scrollRight"><svg id="right-arrow"><use xlink:href="#rightarrow"></use></svg></span>
+		<div class="modal-image">
+			<img src="${currentProject.fullImage}" alt="">
+		</div> <!-- modal image -->
+		<div class="modal-info">
+			<div class="info-text">
+				<h2>${currentProject.title}</h2>
+				<p><strong>The Challenge</strong></p>
+				<p>${currentProject.challenge}</p>
+				<p><strong>Skills Used</strong></p>
+				<p>${currentProject.skill1}</p>
+				<p>${currentProject.skill2}</p>
+				<p>${currentProject.skill3}</p>
+				<p>${currentProject.skill4}</p>
+			</div>
+		</div> <!-- modal-info -->
+		<button class="demo-button"><a href="${currentProject.liveLink}" target="_blank">
+			<div class="modal-button">
+				<svg class="demosvg"><use xlink:href="#demosvg"></use></svg>
+				<p>View Demo</p>
+			</div> <!-- modal-button -->
+		</a></button>
+		<button class="code-button"><a href="${currentProject.githubLink}" target="_blank">
+			<div class="modal-button">
+				<svg class="codesvg"><use xlink:href="#codesvg"></use></svg>
+				<p>See Github</p>
+			</div> <!-- modal-button -->
+		</a></button>`;
+	  modal.style.display = 'block';
+	}
+
+	  $('#portfolio').on('click', 'LI', (function(){
+	    currentIndex = parseInt(this.dataset.index);
+	    scrollCheck();
+	    viewModal(currentIndex);
+
+
+
+		// Close modal
+		$('#close').click(function(){
+		  $('#modal').hide();
+		});
+
+		// Scroll modal
+
+		$('#scrollLeft').click(function(){
+			currentIndex -= 1;
+			scrollCheck();
+			viewModal(currentIndex);
+		});
+
+		$('#scrollRight').click(function(){
+			currentIndex += 1;
+			scrollCheck();
+			viewModal(currentIndex);
+		});
+	}));
--- js/modal.js
+++ js/modal.js
@@ -1,81 +1,10 @@
 // Modal JS
 
-// Open the Modal
-function openModal() {
-  document.getElementById('modal').style.display = "block";
-}
-
-// Close the Modal
-function closeModal() {
-  document.getElementById('modal').style.display = "none";
-}
-
-var projectIndex = 1;
-showProjects(projectIndex);
-
-// Next/previous controls
-function nextProject(n) {
-  showProjects(projectIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showProjects(projectIndex = n);
-}
-
-function showProjects(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {projectIndex = 1}
-  if (n < 1) {projectIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[projectIndex-1].style.display = "block";
-  dots[projectIndex-1].className += " active";
-  captionText.innerHTML = dots[projectIndex-1].alt;
-}
-
-Get Modal Element
-function viewModal(index){
-  modal.style.display = 'block';
-}
-
-  $('#employeeGrid').on('click', 'LI', (function(){
-    currentIndex = parseInt(this.dataset.index);
-    scrollCheck();
-    viewModal(currentIndex);
-}));
-
-// Close modal
-$('#close').click(function(){
-  $('#modal').hide();
-});
-
-// Scroll modal
-
-$('#scrollLeft').click(function(){
-  currentIndex -= 1;
-  scrollCheck();
-  viewModal(currentIndex);
-});
-
-$('#scrollRight').click(function(){
-  currentIndex += 1;
-  scrollCheck();
-  viewModal(currentIndex);
-});
-
-const employeeGrid = document.getElementById('employeeGrid');
+const portfolio = document.getElementById('portfolio');
 const modal = document.getElementById('modal');
 const userInfoApi = 'https://johnskeen.github.io/projects.json';
 let currentIndex = 0;
-let employeeData = [];
+let projectData = [];
 
 // ------------------------------------------
 //  FETCH FUNCTIONS
@@ -88,34 +17,33 @@ function fetchData(url) {
             .catch(error => console.log('Looks like there was a problem!', error));
 }
 
-fetchData(userInfoApi)
+fetchData(projectInfo)
   .then(data => {
-    let employeeInfo = "<ul id='employeeList'>";
+    let projectInfo = "<ul id='gridContainer'>";
 
     $.each (data.results, function (key, value) {
-      employeeInfo += `
-      <li class="employee" data-index="` + key + `">
-        <img class="employeeImage" src="${value.picture.large}">
-        <div class="employeeText">
-          <h3 class="employeeName">${value.name.first} ${value.name.last}</h3>
-          <p>${value.email}</p>
-          <p>${value.location.city}</p>
-        </div>
+      projectInfo += `
+      <li class="${value.class} project" data-index="` + key + `">
+        ${value.thumbnail}
       </li>`
 
-      let employeeResults = {
-        image: value.picture.large,
-        name: value.name.first + ' ' + value.name.last,
-        email: value.email,
-        city: value.location.city,
-        phone: value.phone,
-        address: value.location.street + ', ' + value.location.state + ' ' + value.location.postcode,
-        birthday: 'Birthday: ' + value.dob.date.substring(5,7) + '/' + value.dob.date.substring(8,10) + '/' + value.dob.date.substring(0,4)
+      let portfolioResults = {
+        title: value.title,
+        challenge: value.challenge,
+        skill1: value.skill1,
+        skill1: value.skill2,
+        skill1: value.skill3,
+        skill1: value.skill4,
+        fullImage: value.fullImage,
+        thumbnail: value.thumbnail,
+        class: value.class,
+        githubLink: value.githubLink,
+        liveLink: value.liveLink
       };
-      employeeData.push(employeeResults);
+      projectData.push(portfolioResults);
     });
-    employeeInfo += `</ul>`;
-    employeeGrid.innerHTML = employeeInfo;
+    projectInfo += `</ul>`;
+    portfolio.innerHTML = projectInfo;
   })
   .catch(console.log('This does not seem to be working!'));
 
